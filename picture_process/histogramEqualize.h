0d1a8630da957dd4eae57aa04750cf0289c34c49
root <root@localhost.localdomain>
2023-02-26 21:00:38 +0800

    Add picture process

---
+++ picture_process/histogramEqualize.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include "stdlib.h"
+#include "stdio.h"
+#include <math.h>
+#include "threshold.h"
+#include "spatialFilter.h"
+
+class HistogramEqualize {
+public:
+	int grayPoint[256];
+	float grayChance[256] = { 0 };
+
+	void histogram(unsigned char * bmp, int width, int height, int linebyte);
+
+	void chance_gery(unsigned char * bmp, int width, int height, int linebyte);
+
+	unsigned char * saveHistogram(unsigned char * bmp, int width, int height, int linebyte);
+
+	unsigned char * equalization(unsigned char * bmp, int width, int height, int linebyte);
+};
+
---
+++ picture_process/splitBmp.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include "stdlib.h"
+#include "stdio.h"
+
+using namespace std;
+
+typedef struct rgb {
+	unsigned char * red;
+	unsigned char * green;
+	unsigned char * blue;
+};
+
+class SplitBmp {
+public:
+	int i, j;
+
+	rgb init_rgb(unsigned char * bmp, int height, int width, int linebyte);
+
+	unsigned char * init_gray(unsigned char * bmp, int height, int width, int linebyte, int lineByte2);
+
+	unsigned char * init_opposite_gray(unsigned char * bmp, int height, int width, int linebyte);
+};
+
